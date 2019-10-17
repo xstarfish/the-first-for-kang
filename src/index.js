49d167b1819676fcbfa06c5c45507e8163f0c06a
xstarfish <1060614445@qq.com>
2019-10-18 02:39:17 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import useTypewriter from "react-typewriter-hook";
+import { random } from "lodash";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const MagicOcean = [
+    "I love you~~~",
+    "小宝贝，我看你现在睡得那么香",
+    "我有一些话想对你讲~~~~",
+    "我爱你哟~~康康宝贝 mua~~"
+];
+let index = 0;
+
+function App() {
+    const [magicName, setMagicName] = useState("magic is going to happen");
+    const intervalRef = useRef({});
+    const name = useTypewriter(magicName);
+    useEffect(
+        () => {
+            intervalRef.current = setInterval(() => {
+                // index = index + 1 > 2 ? 0 : ++index + 1;
+                index = index > 3 ? 0 : ++index;
+                setMagicName(MagicOcean[index]);
+            }, 5000);
+            return function clear() {
+                clearInterval(intervalRef.current);
+            };
+        },
+        [magicName]
+    );
+    return (
+        <div className="App">
+            <h1>Hello CodeSandbox</h1>
+            <h2>Start editing to see some magic happen!</h2>
+            <p className="cursor">{name}</p>
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
