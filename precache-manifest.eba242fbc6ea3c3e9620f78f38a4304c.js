ce9af177b533e21edd9723cb369a0532b16085b1
xstarfish <1060614445@qq.com>
2019-10-18 02:55:29 +0800

    Updates

---
+++ precache-manifest.eba242fbc6ea3c3e9620f78f38a4304c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2c68c10859d8aaad24fd4cf6b48f7b",
+    "url": "/the-first-for-kang/index.html"
+  },
+  {
+    "revision": "7ff43f29954ae0aa5115",
+    "url": "/the-first-for-kang/static/css/main.fe226763.chunk.css"
+  },
+  {
+    "revision": "b2d494867cb0fa3b2909",
+    "url": "/the-first-for-kang/static/js/2.70362167.chunk.js"
+  },
+  {
+    "revision": "7ff43f29954ae0aa5115",
+    "url": "/the-first-for-kang/static/js/main.d1446991.chunk.js"
+  },
+  {
+    "revision": "164453812fe9b45529bc",
+    "url": "/the-first-for-kang/static/js/runtime-main.25f01a19.js"
+  }
+]);
